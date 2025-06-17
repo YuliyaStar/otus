0191b9d6d71cd5289ad6f3e9a15eefd65c3c39ce
Julia Starostina <juliakabanova@MacBook-Pro.local>
2025-06-17 22:41:34 +0400

    добавление проверки теоремы неравенства треугольника

--- src/triangle.py
+++ src/triangle.py
@@ -3,8 +3,11 @@ from src.figure import Figure
 
 class Triangle(Figure):
     def __init__(self, side_a, side_b, side_c):
+
         if side_a <= 0 or side_b <= 0 or side_c <= 0:
             raise ValueError("Triangle sides can't be less than 0")
+        if not (side_a + side_b > side_c and side_a + side_c > side_b and side_b + side_c > side_a):
+            raise ValueError("it is impossible to create a triangle")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
@@ -13,7 +16,6 @@ class Triangle(Figure):
     def get_perimeter(self):
         return self.side_a + self.side_b + self.side_c
 
-    @property
     def get_area(self):
         """
         p - полупериметр,
